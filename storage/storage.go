a1d0cd5aee07425e94f6886c98795efa6027cb41
Emre <emrekayan@gmail.com>
2015-07-01 16:11:18 +0300

    add 2 new methods to storage struct

--- storage/storage.go
+++ storage/storage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 	// "errors"
 
@@ -13,13 +14,13 @@ type Storage struct {
 	pool *redis.Pool
 }
 
-func (stg *Storage) AddSubscriber(appId string, channelId string, subscriberIds []string) {
+func (stg *Storage) AddSubscriber(appID string, channelID string, subscriberIDs []string) {
 	conn := stg.pool.Get()
 	defer conn.Close()
 
-	stg.AddChannel(appId, channelId)
-	subscribersKey := addPrefix("apps." + appId + ".channels." + channelId + ".subscribers")
-	tmpParams := append([]string{subscribersKey}, subscriberIds...)
+	stg.AddChannel(appID, channelID)
+	subscribersKey := addPrefix("apps." + appID + ".channels." + channelID + ".subscribers")
+	tmpParams := append([]string{subscribersKey}, subscriberIDs...)
 
 	params := make([]interface{}, len(tmpParams))
 	for i, v := range tmpParams {
@@ -29,66 +30,123 @@ func (stg *Storage) AddSubscriber(appId string, channelId string, subscriberIds 
 	conn.Do("SADD", params...)
 }
 
-func (stg *Storage) AddChannel(appId string, channelId string) {
+func (stg *Storage) AddChannel(appID string, channelID string) {
 	conn := stg.pool.Get()
 	defer conn.Close()
 
-	channelsKey := addPrefix("apps." + appId + ".channels")
-	conn.Do("SADD", channelsKey, channelId)
+	channelsKey := addPrefix("apps." + appID + ".channels")
+	conn.Do("SADD", channelsKey, channelID)
 }
 
-func (stg *Storage) DeleteChannel(appId string, channelId string) {
+func (stg *Storage) DeleteChannel(appID string, channelID string) {
 	conn := stg.pool.Get()
 	defer conn.Close()
 
-	channelsKey := addPrefix("apps." + appId + ".channels")
-	conn.Do("SREM", channelsKey, channelId)
+	channelsKey := addPrefix("apps." + appID + ".channels")
+	conn.Do("SREM", channelsKey, channelID)
 
-	channelKey := channelsKey + "." + channelId + ".subscribers"
+	channelKey := channelsKey + "." + channelID + ".subscribers"
 	conn.Do("DEL", channelKey)
 }
 
-func (stg *Storage) AddSubscriberDevice(appId string, subscriberId string, device *Device) {
+func (stg *Storage) AddSubscriberDevice(appID string, subscriberID string, device *Device) error {
 	conn := stg.pool.Get()
 	defer conn.Close()
 
-	subscribersKey := addPrefix("apps." + appId + ".subscribers")
-	conn.Do("SADD", subscribersKey, subscriberId)
+	subscribersKey := addPrefix("apps." + appID + ".subscribers")
+	conn.Do("SADD", subscribersKey, subscriberID)
 
-	devicesKey := subscribersKey + "." + subscriberId + ".devices"
+	devicesKey := subscribersKey + "." + subscriberID + ".devices"
 	jstring, _ := json.Marshal(device)
 	// todo: multiple devices with same platform and token should not be added
-	conn.Do("SADD", devicesKey, jstring)
+	_, err := conn.Do("HSET", devicesKey, device.Token, jstring)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (stg *Storage) AppExists(appId string) bool {
+func (stg *Storage) UpdateDeviceToken(appID string, subscriberID string, oldDeviceToken string, newDeviceToken string) error {
 	conn := stg.pool.Get()
 	defer conn.Close()
 
-	status, err := redis.Int(conn.Do("SISMEMBER", addPrefix("apps"), appId))
-	_ = err
-	if status == 0 {
-		return false
-	} else {
-		return true
+	key := addPrefix("apps." + appID + ".subscribers." + subscriberID + ".devices")
+	deviceData, err := redis.String(conn.Do("HGET", key, oldDeviceToken))
+	if err != nil {
+		return err
 	}
+
+	conn.Do("HDEL", key, oldDeviceToken)
+
+	var device Device
+	decoder := json.NewDecoder(strings.NewReader(deviceData))
+
+	if err := decoder.Decode(&device); err != nil {
+		return err
+	}
+
+	device.Token = newDeviceToken
+	err = stg.AddSubscriberDevice(appID, subscriberID, &device)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (stg *Storage) CreateApp(appId string, appData string) {
+func (stg *Storage) GetSubscriberDevices(appID string, subscriberID string) ([]Device, error) {
+	conn := stg.pool.Get()
+	defer conn.Close()
+
+	key := addPrefix("apps." + appID + ".subscribers." + subscriberID + ".devices")
+
+	var devices map[string]string
+	devices, err := redis.StringMap(conn.Do("HGETALL", key))
+	if err != nil {
+		return nil, err
+	}
+
+	var device Device
+	var response []Device
+	for _, deviceData := range devices {
+		decoder := json.NewDecoder(strings.NewReader(deviceData))
+		decoder.Decode(&device)
+		response = append(response, device)
+	}
+
+	return response, nil
+}
+
+func (stg *Storage) AppExists(appID string) bool {
+	conn := stg.pool.Get()
+	defer conn.Close()
+
+	status, err := redis.Int(conn.Do("SISMEMBER", addPrefix("apps"), appID))
+
+	if status == 0 || err != nil {
+		return false
+	}
+
+	return true
+}
+
+func (stg *Storage) CreateApp(appID string, appData string) {
 	conn := stg.pool.Get()
 	defer conn.Close()
 
 	appsKey := addPrefix("apps")
-	conn.Do("SADD", appsKey, appId)
-	appKey := appsKey + "." + appId
+	conn.Do("SADD", appsKey, appID)
+	appKey := appsKey + "." + appID
 	conn.Do("SET", appKey, appData)
 }
 
-func (stg *Storage) GetApp(appId string) (string, error) {
+func (stg *Storage) GetApp(appID string) (string, error) {
 	conn := stg.pool.Get()
 	defer conn.Close()
 
-	appKey := addPrefix("apps") + "." + appId
+	appKey := addPrefix("apps") + "." + appID
 	value, err := redis.String(conn.Do("GET", appKey))
 	if err != nil {
 		return "", err
