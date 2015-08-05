324f56b7cd76639a9036305abc6fa9a652ab8481
Mustafa Altun <gmail@mustafaaltun.com>
2015-08-05 14:15:40 +0300

    Group storage methods

--- storage/interface.go
+++ storage/interface.go
@@ -1,26 +1,7 @@
 package storage
 
 type Storage interface {
-	// AddSubscriber adds new subscriber to channel.
-	AddSubscriber(appID string, channelID string, subscriberIDs []string) error
-
-	// AddChannel adds new channel to app.
-	AddChannel(appID string, channelID string) error
-
-	// DeleteChannel deletes channel and its subscribers from app.
-	DeleteChannel(appID string, channelID string) error
-
-	// AddSubscriberDevice adds new device to subscriber.
-	AddSubscriberDevice(appID string, subscriberID string, device *Device) error
-
-	// UpdateDeviceToken updates token of a subscriber's device.
-	UpdateDeviceToken(appID string, subscriberID string, oldDeviceToken string, newDeviceToken string) error
-
-	// GetChannelSubscribers gets subscribers of a channel.
-	GetChannelSubscribers(appID string, channelID string) ([]string, error)
-
-	// GetSubscriberDevices gets devices of a subscriber.
-	GetSubscriberDevices(appID string, subscriberID string) ([]Device, error)
+	// App methods
 
 	// AppExists tells whether an app exists or not.
 	AppExists(appID string) bool
@@ -30,4 +11,29 @@ type Storage interface {
 
 	// GetApp gets an app's data.
 	GetApp(appID string) (*App, error)
+
+	// Subscriber methods
+
+	// AddSubscriberDevice adds new device to subscriber.
+	AddSubscriberDevice(appID string, subscriberID string, device *Device) error
+
+	// UpdateDeviceToken updates token of a subscriber's device.
+	UpdateDeviceToken(appID string, subscriberID string, oldDeviceToken string, newDeviceToken string) error
+
+	// GetSubscriberDevices gets devices of a subscriber.
+	GetSubscriberDevices(appID string, subscriberID string) ([]Device, error)
+
+	// Channel methods
+
+	// AddSubscriber adds new subscriber to channel.
+	AddSubscriber(appID string, channelID string, subscriberIDs []string) error
+
+	// AddChannel adds new channel to app.
+	AddChannel(appID string, channelID string) error
+
+	// DeleteChannel deletes channel and its subscribers from app.
+	DeleteChannel(appID string, channelID string) error
+
+	// GetChannelSubscribers gets subscribers of a channel.
+	GetChannelSubscribers(appID string, channelID string) ([]string, error)
 }
